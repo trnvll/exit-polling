62de0350e89c174a0506715032519e04c2240d2b
Alexander Törnvall <alexander.tornvall@roycgroup.com>
2022-12-30 20:02:25 +0100

    chore: database schema, migration + changeset functionality

--- poller_dal/priv/repo/migrations/20221229102628_create_districts.exs
+++ poller_dal/priv/repo/migrations/20221229102628_create_districts.exs
@@ -2,6 +2,10 @@ defmodule PollerDal.Repo.Migrations.CreateDistricts do
   use Ecto.Migration
 
   def change do
+    create table(:districts) do
+      add(:name, :string)
 
+      timestamps()
+    end
   end
 end
