4008581265dc2b8868df5749281d7f475b0287f7
Alexander Törnvall <alexander.tornvall@roycgroup.com>
2022-12-31 21:49:26 +0100

    chore: hash user password + admin changeset functionality

--- poller_dal/mix.exs
+++ poller_dal/mix.exs
@@ -23,7 +23,8 @@ defmodule PollerDal.MixProject do
   defp deps do
     [
       {:ecto_sql, "~> 3.9"},
-      {:postgrex, "~> 0.16.5"}
+      {:postgrex, "~> 0.16.5"},
+      {:argon2_elixir, "~> 3.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
